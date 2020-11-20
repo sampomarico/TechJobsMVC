c3f087a35a2f2ea8dc02660144cdfd100c1d23e3
Sam <samtpomarico@gmail.com>
2020-11-19 20:33:45 -0600

    Added bonus 1

--- TechJobsMVC/Controllers/SearchController.cs
+++ TechJobsMVC/Controllers/SearchController.cs
@@ -23,6 +23,7 @@ namespace TechJobsMVC.Controllers
         public IActionResult Results(string searchType, string searchTerm)
         {
             List<Job> jobs;
+            ViewBag.searchTerm = searchTerm;
             if(searchTerm == null)
             {
                 jobs =  JobData.FindAll();
